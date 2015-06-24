e2e480dcac93338bd1cd3a58cb7b77551385d6a5
Martin Fillafer <martin.fillafer@bitmovin.net>
2015-06-24 15:37:25 +0200

    add job tests

---
+++ test/bitcodin.job.test.js
@@ -0,0 +1,59 @@
+/**
+ * Created by mfillafer on 23.06.15.
+ */
+
+var util = require('./util'),
+    bitcodin = require('../lib/bitcodin')(util.settings.apiKey);
+
+describe('Job', function () {
+    var jobIds = [];
+
+    it('should create a new job', function () {
+        var jobConfig = {
+            'inputId': 3315,
+            'encodingProfileId': 7365,
+            'manifestTypes': [
+                'mpd',
+                'm3u8'
+            ]
+        };
+
+        var promise = bitcodin.job.create(jobConfig);
+
+        promise.then(function (data) {
+            jobIds.push(data.jobId);
+        });
+
+        return promise.should.eventually.be.fulfilled;
+    });
+
+    it('should list jobs', function () {
+        return bitcodin.job.list().should.eventually.be.fulfilled;
+    });
+
+    it('should list jobs of a given page', function () {
+        var page = 2;
+        return bitcodin.job.list(page).should.eventually.be.fulfilled;
+    });
+
+    it('should get the job details for a given job id', function () {
+        return bitcodin.job.getDetails(2874).should.eventually.be.fulfilled;
+    });
+
+    it('should get the job status for a given job id', function () {
+        return bitcodin.job.getStatus(2874).should.eventually.be.fulfilled;
+    });
+
+    it('should create a new transfer job', function () {
+        var transferJobConfig = {
+            'jobId': 2874,
+            'outputId': 2564
+        };
+
+        return bitcodin.job.transfer.create(transferJobConfig).should.eventually.be.fulfilled;
+    });
+
+    it('should get the transfer job details for a given id', function () {
+        return bitcodin.job.transfer.list(2874).should.eventually.be.fulfilled;
+    });
+});
